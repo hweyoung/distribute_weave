3e178107f19b4a2d2603d886ee2056c4c92f9b09
hweyoung <gnldud0516@gmail.com>
2022-07-22 01:27:06 +0900

    Build : 22.07.22 build File

--- src/main/java/com/weave/weaveserver/WeaveServerApplication.java
+++ src/main/java/com/weave/weaveserver/WeaveServerApplication.java
@@ -2,13 +2,18 @@ package com.weave.weaveserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class WeaveServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeaveServerApplication.class, args);
 	}
 
+//	@Bean
+//	public BCryptPasswordEncoder encodePwd() {
+//		return new BCryptPasswordEncoder();
+//	}
 }
